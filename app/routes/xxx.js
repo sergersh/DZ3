3e72fe43886c97e2faba6fb0c63fc87ffdb079cd
sergershe/emberDZ <sergersh@yandex.ru>
2021-11-22 23:04:18 +0500

    DZ2

--- app/routes/xxx.js
+++ app/routes/xxx.js
@@ -1,8 +1,28 @@
 import Route from '@ember/routing/route';
 
 export default class XxxRoute extends Route {
-    async mode() {
-        const response = await fetch('http://localhost:3000/books')
-        return response.json();
-    }
+    model(){
+    //async mode() {
+        //<!--const response = await fetch('http://localhost:3000/books')
+        //return response.json();-->
+        returen [
+            
+                { 
+                    Name: '1',
+                    Authors: 'А',
+                    Pages: '100',
+                },
+                {                   
+                    Name: '2',
+                    Authors: 'А',
+                    Pages: '100',
+                },
+                {                    
+                    Name: '3',
+                    Authors: 'А',
+                    Pages: '100',
+                }
+            
+              ];
+         }
 }
